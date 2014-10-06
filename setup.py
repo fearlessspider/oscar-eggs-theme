afca3597d80670434045551f0b3e491a34f5872b
Przemyslaw Pajak <bespider@gmail.com>
2014-10-07 01:04:30 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='oscar-eggs-theme',
+    version='0.0.1',
+    packages=['eggs'],
+    url='http://www.eggsforsale.com',
+    license='CC BY 3.0 NC',
+    author='fearless',
+    author_email='support@eggforsale.com',
+    description='Eggs is theme for django oscar ecommerce based on Foundation 5.x.'
+)
