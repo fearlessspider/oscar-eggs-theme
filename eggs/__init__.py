e4b40a927c088d6edf641c47879d7a78b7427c13
Przemyslaw Pajak <bespider@gmail.com>
2014-10-12 22:29:20 +0200

    Revert "Merge branch 'master' into develop"
    This reverts commit a5b2fabe85549aa220ed6d86f5ffcceebb81dbcf, reversing
    changes made to cc60ac100e9908ce33f2230f5d04bd35cf79c997.

--- eggs/__init__.py
+++ eggs/__init__.py
@@ -1,1 +1,19 @@
 __author__ = 'fearless'
+
+VERSION = (0, 0, 1, 'dev')
+
+
+def get_short_version():
+    return '%s.%s' % (VERSION[0], VERSION[1])
+
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    # Append 3rd digit if > 0
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    elif VERSION[3] != 'final':
+        version = '%s %s' % (version, VERSION[3])
+        if len(VERSION) == 5:
+            version = '%s %s' % (version, VERSION[4])
+    return version
