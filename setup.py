ef47ee5a3fa8332f0945e1cc579b9de0f3d285ff
Przemyslaw Pajak <bespider@gmail.com>
2014-10-12 22:31:00 +0200

    fixed commit

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     license='CC BY 3.0 NC',
     author='fearless',
     author_email='support@eggforsale.com',
-    description='Eggs is theme for django oscar ecommerce based on Foundation 5.x.', requires=['django','django-oscar']
+    description='Eggs is theme for django oscar ecommerce based on Foundation 5.x.', requires=['django']
 )
